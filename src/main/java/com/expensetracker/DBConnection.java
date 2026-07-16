a8dcc480cd7373c0de913709d38d0382d485326e
LalithaDevi-P <lalithap6410@gmail.com>
2026-07-16 16:15:15 +0530

    Add postgres database connection

--- src/main/java/com/expensetracker/DBConnection.java
+++ src/main/java/com/expensetracker/DBConnection.java
@@ -1,4 +1,16 @@
 package com.expensetracker;
+import java.sql.*;
 
 public class DBConnection {
+    private static final String url = "jdbc:postgresql://localhost:5432/expense-tracker";
+    private static final String uname = "postgres";
+    private static final String password = "Maname@2004";
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, uname, password);
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
 }
